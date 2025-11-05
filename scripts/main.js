70d082044bbd6a2aba3eb57576cc18aeb39cbed7
Quirin <quirin.pflaum@gmx.ch>
2025-11-05 20:34:17 +0100

    rendering off pure content without buttons and imgs

--- scripts/main.js
+++ scripts/main.js
@@ -30,10 +30,49 @@ console.log('REGION_URL abs:', new URL(REGION_URL, location.href).href);
 const menuRes = await fetch(MENU_URL);
 const menu = await menuRes.json();                    // { categories: [...], items: [...] }
 
+renderLinkBar();
+renderMenu();
+
 // check if there TO BE DELETED
 // console.table(menu);
 // console.table(menu.categories);
 // console.log('Items:', menu.items.length, 'Restaurant:', restaurant.name);
 
 //die menu rendern
+function renderMenu() {
+    const ITEMS = menu.items;
+    let current = null;
+    let html = '';
+    ITEMS.forEach(it => {
+        if (it.category !== current) {
+            current = it.category;
+            html += `<h2 id="${current}" class="-dist16px>${current}</h2><div>`;
+        }
+        html += `
+      <article class="meneu-item">
+        <h4>${it.name}</h4>
+        <p>${it.desc}</p>
+        <div class="meta">
+          <span class="portion">${it.portion || ''}</span>
+          <span class="price">CHF ${(it.priceCents / 100).toFixed(2)}</span>
+        </div>
+      </article>`;
+    })
+    html+= `</div>`;
+    $('#content').innerHTML = html;
+}
 
+
+
+
+function renderLinkBar() {
+    const MENULINK = menu.categories;
+    let html = '';
+    MENULINK.forEach(CAT => {
+        const ID = CAT.id;
+        const NAME = CAT.name;
+        html += `<a href="${ID}" class="btn">${NAME}</a>`;
+    });
+    $('#menu-bar').innerHTML = html;
+}
+//
